2ec768ed0925d2d4003f28b0282564c8b5aec061
Imam Gig <rinoneo666@gmail.com>
2023-07-15 14:01:38 +0300

    HW 9

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+// void PrintNum(int n)
+// {
+//     if (n >= 1)
+//     {
+//         PrintNum(n - 1);
+//         System.Console.WriteLine(n + " ");
+//     }
+// }
+// PrintNum(8);
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// int SumMN(int m, int n)
+// {
+//     int res = m;
+//     if (m == n )
+//         return 0;
+//     else
+//     {
+//         m++;
+//         res = m + SumMN(m, n);
+//         return res;
+//     }
+// }
+// System.Console.WriteLine(SumMN(1,15));
+
+
+// int Akkerman(int m, int n)
+// {
+//     if (m == 0)
+//     {
+//         return n + 1;
+//     }
+//     else if (n == 0 && m > 0)
+//     {
+//         return Akkerman(m - 1, 1);
+//     }
+//     else
+//     {
+//         return (Akkerman(m - 1, Akkerman(m, n - 1)));
+//     }
+// }
+
+ 
+//  System.Console.WriteLine(Akkerman(2, 3));
